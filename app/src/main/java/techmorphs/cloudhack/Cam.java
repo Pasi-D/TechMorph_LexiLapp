42c6561e5b148ee5e35c27fac3be48a8f89f043f
nimesha95 <nimesha95@live.com>
2017-08-12 13:17:29 +0530

    Removed activity bar in camera view

--- app/src/main/java/techmorphs/cloudhack/Cam.java
+++ app/src/main/java/techmorphs/cloudhack/Cam.java
@@ -1,14 +1,43 @@
 package techmorphs.cloudhack;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Cam extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+    public byte [] thePictureByteArray;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        Log.d("hello", ""+data.length);
+        thePictureByteArray =new byte[data.length];
+        for(int i=0;i<data.length;i++){
+            thePictureByteArray[i]=127;
+            Log.d("hello",""+thePictureByteArray[i]);
+        }
+        System.arraycopy(data,0,thePictureByteArray,0,data.length);
+       // thePictureByteArray=data;
+        Log.d("hello", "done"+thePictureByteArray[3000]);
+    }
+};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +51,22 @@ public class Cam extends AppCompatActivity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.camera_button_rel);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                        //Log.d("hello1",""+thePictureByteArray.length);
+                        for(int i=0;i<thePictureByteArray.length;i++){
+                            Log.d("assignedFuck"," "+i+" "+thePictureByteArray[i]);
+                        }
+                    }
+                }
+        );
     }
 
 
