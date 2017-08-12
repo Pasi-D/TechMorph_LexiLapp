3c53a2da4ddab058ed58873e447c793b468dc3ff
nimesha95 <nimesha95@live.com>
2017-08-12 09:41:45 +0530

    added activity to camera view

---
+++ app/src/main/java/techmorphs/cloudhack/Cam.java
@@ -0,0 +1,13 @@
+package techmorphs.cloudhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+    }
+}
