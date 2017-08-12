2c5114b489dc8a5aa22e3644f32b2d3a4a258f19
nimesha95 <nimesha95@live.com>
2017-08-12 11:06:14 +0530

    camera is workint

--- app/src/main/java/techmorphs/cloudhack/MainActivity.java
+++ app/src/main/java/techmorphs/cloudhack/MainActivity.java
@@ -1,7 +1,10 @@
 package techmorphs.cloudhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button next1 =(Button)findViewById(R.id.button);
+        next1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this,Cam.class);
+                startActivity(i);
+            }
+        });
     }
 }
