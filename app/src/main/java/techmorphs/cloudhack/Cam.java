b0e13bc09d7cee183e6dae69df7c54bde62269b8
nimesha95 <nimesha95@live.com>
2017-09-15 20:57:15 +0530

    Sends keywords to the server

--- app/src/main/java/techmorphs/cloudhack/Cam.java
+++ app/src/main/java/techmorphs/cloudhack/Cam.java
@@ -1,25 +1,38 @@
 package techmorphs.cloudhack;
 
-import android.content.Intent;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 import java.util.Map;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class Cam extends AppCompatActivity {
-    public static DatabaseHelper db;
+    public static Item returnedItem = null;
+    public String keyword_server = "";
+    public byte[] thePictureByteArray;
     private Camera mCamera;
     private CameraPreview mPreview;
-    public byte [] thePictureByteArray;
-    public static Item returnedItem=null;
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] imageBytes, Camera camera) {
@@ -33,26 +46,15 @@ public class Cam extends AppCompatActivity {
                     Log.d("gcp", "cloud vision annotations:" + annotations);
 
                     String[] keyWords = annotations.keySet().toArray(new String[annotations.keySet().size()]);
-                    //if keyWords
 
-                    //int n=DatabaseHelper.selectBestMatchingItem(keyWords);
-                    Log.d("pppp",""+keyWords[0]);
-
-                    for (String s :keyWords){
-                        if (s.equals("soft drink")||s.equals("green")){
-                            Log.d("FINAL","sprite");
-                            returnedItem=DatabaseHelper.selectBestMatchingItem(1);
-                            break;
-                        }
-                        else if (s.equals("bottled water")||s.equals("mineral water")){
-                            Log.d("FINAL","bottleOfWater");
-                            returnedItem=DatabaseHelper.selectBestMatchingItem(2);
-                            break;
-                        }
-
+                    for (String s : keyWords) {
+                        //Log.d("ppx",s);
+                        keyword_server += " " + s;
                     }
-                    Intent i = new Intent(Cam.this, MainActivity.class);
-                    startActivity(i);
+
+                    Log.d("pppp", "" + keyword_server);
+                    new SendPostRequest().execute(keyword_server);
+
 /*
                     if(returnedItem == null){
                         Toast.makeText(Cam.this, "Item Not found! Please rescan",
@@ -77,6 +79,19 @@ public class Cam extends AppCompatActivity {
             }
         }
     };
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,8 +125,10 @@ public class Cam extends AppCompatActivity {
                         //Intent i = new Intent(Cam.this, abc.class);
                         //startActivity(i);
                         mCamera.takePicture(null, null, mPicture);
-                        Log.d("hello1","12");
+                        Log.d("hello1", "12");
 
+
+                        /*
                         Handler mHandler = new Handler();   //this method is not good... need to switch activity after receving the json
                         mHandler.postDelayed(new Runnable() {
 
@@ -122,6 +139,10 @@ public class Cam extends AppCompatActivity {
                             }
 
                         }, 5000L);
+*/
+
+
+
 
                         //Intent intent = new Intent(Cam.this, NavNavDrawer.class);
                         //startActivity(intent);
@@ -141,7 +162,6 @@ public class Cam extends AppCompatActivity {
         releaseCamera();              // release the camera immediately on pause event
     }
 
-
     private void releaseCamera(){
         if (mCamera != null){
             mCamera.release();        // release the camera for other applications
@@ -149,16 +169,98 @@ public class Cam extends AppCompatActivity {
         }
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while (itr.hasNext()) {
+
+            String key = itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
         }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
+        return result.toString();
+    }
+
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        protected void onPreExecute() {
         }
-        return c; // returns null if camera is unavailable
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                URL url = new URL("http://35.197.26.101"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("keywords", arg0[0]);
+                //postDataParams.put("email", "abc@gmail.com");
+                Log.e("params", postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in = new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line = "";
+
+                    while ((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                } else {
+                    return new String("false : " + responseCode);
+                }
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Log.d("helo");
+            Toast.makeText(Cam.this, result,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
 }
--- app/src/main/java/techmorphs/cloudhack/ItemSelector.java
+++ app/src/main/java/techmorphs/cloudhack/ItemSelector.java
@@ -34,7 +34,6 @@ public class ItemSelector {
                 maxE = e;
             }
         }
-
         return (int) maxE.getKey();
     }
 
